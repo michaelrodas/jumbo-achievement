8d77a2cf53cbc6d0d2266175e02e7dc5ac2d9d4e
michaelrodas <hollowlife7@gmail.com>
2020-03-03 10:04:04 +0100

    Lombok library is included

--- StoresLocator/src/main/java/com/jumbo/achievement/StoresLocator/dao/StoreDAO.java
+++ StoresLocator/src/main/java/com/jumbo/achievement/StoresLocator/dao/StoreDAO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jumbo.achievement.StoresLocator.dto.JsonStore;
 import com.jumbo.achievement.StoresLocator.dto.Store;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
@@ -18,9 +19,9 @@ import java.util.Collection;
 /**
  * Created by MichaelR on 8/9/2018.
  */
+@Slf4j
 @Repository
 public class StoreDAO implements IStoreDAO{
-    private static final Logger logger = LoggerFactory.getLogger(StoreDAO.class);
 
     @Override
     @Cacheable("stores")
@@ -31,11 +32,11 @@ public class StoreDAO implements IStoreDAO{
         TypeReference<JsonStore> typeReference = new TypeReference<JsonStore>(){};
         InputStream inputStream = TypeReference.class.getResourceAsStream("/json/stores.json");
         try {
-            logger.info("Loading stores from JSON file...");
+            log.info("Loading stores from JSON file...");
             JsonStore jsonStore =  mapper.readValue(inputStream,typeReference);
             stores = new ArrayList<>(jsonStore.getStores());
         } catch (IOException e){
-            logger.error("Unable to load JSON file: " + e.getMessage());
+            log.error("Unable to load JSON file: " + e.getMessage());
         }
         return stores;
     }
