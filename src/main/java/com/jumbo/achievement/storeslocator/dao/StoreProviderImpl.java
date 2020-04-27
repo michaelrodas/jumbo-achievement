556ad8fb34b12ed312006bb51d7ba0d1c281933a
Mike <hollowlife7@gmail.com>
2020-04-27 12:07:16 +0200

    StoreProviderImpl code is simplified

--- src/main/java/com/jumbo/achievement/storeslocator/dao/StoreProviderImpl.java
+++ src/main/java/com/jumbo/achievement/storeslocator/dao/StoreProviderImpl.java
@@ -1,6 +1,5 @@
 package com.jumbo.achievement.storeslocator.dao;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jumbo.achievement.storeslocator.dto.JsonStore;
 import com.jumbo.achievement.storeslocator.dto.Store;
@@ -10,12 +9,11 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static com.fasterxml.jackson.databind.DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY;
 import static java.util.Collections.unmodifiableList;
 
 @Slf4j
@@ -32,16 +30,14 @@ class StoreProviderImpl implements StoreProvider {
     @Cacheable("stores")
     public Collection<Store> getAllStores() {
         List<Store> stores = new ArrayList<>();
-        final ObjectMapper mapper = new ObjectMapper().configure(ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        final TypeReference<JsonStore> typeReference = new TypeReference<JsonStore>(){};
 
         try {
             log.info("Loading stores from JSON file...");
-            final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(storesFilePath);
-            final JsonStore jsonStore =  mapper.readValue(inputStream,typeReference);
+            URL sourceFile = getClass().getClassLoader().getResource(storesFilePath);
+            final JsonStore jsonStore = new ObjectMapper().readValue(sourceFile, JsonStore.class);
             stores = unmodifiableList(jsonStore.getStores());
-        } catch (IOException | IllegalArgumentException e){
-            log.error("Unable to load JSON file: " + e.getMessage());
+        } catch (IOException | IllegalArgumentException e) {
+            log.error("Unable to load JSON file {}: ", storesFilePath, e);
         }
         return stores;
     }
